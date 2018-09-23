82e354d9579555fd979c8d0b2bd38913f5f8c65f
johan974 <jj974@upcmail.nl>
2018-09-23 15:52:33 +0200

    Log maven dependencies added.

--- module3/src/functionaltest/java/cucumbertest/UserSteps.java
+++ module3/src/functionaltest/java/cucumbertest/UserSteps.java
@@ -1,15 +1,15 @@
 package cucumbertest;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import users.User;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class UserSteps {
     Logger logger = LoggerFactory.getLogger(UserSteps.class);
